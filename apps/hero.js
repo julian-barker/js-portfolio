81d27ecb14a0a08cdf89bcd0f8157e8d30f7c20b
Julian Barker <julian.c.barker@gmail.com>
2022-09-13 16:09:20 -0700

    add project cards

--- apps/hero.js
+++ apps/hero.js
@@ -2,7 +2,6 @@
 
 // declare shortcut function
 function $(selector) { return document.querySelector(selector); }
-function byId(id) { return document.getElementById(id); }
 // eslint-disable-next-line no-unused-vars
 function _(tag) { return document.createElement(tag); }
 
